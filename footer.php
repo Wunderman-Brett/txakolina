9d55b90ae4730cb690cae0f8898a03123b6ac1bc
Peter Mumford <peter.mumford@fmc-na.com>
2018-01-24 09:10:23 -0500

    removed some css errors. trying to get respond.js to work. also testing updates from github

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-        
+
         <?php if ( get_theme_mod( 'txakolina_custom_instagram' ) ) { ?>
         <div class="txakolina-instagram" data-inst="<?php echo esc_html( get_theme_mod( 'txakolina_custom_instagram' ) ); ?>">
             <h3 class="txakolina-instagram-title"><?php esc_html_e( 'Follow me on Instagram!', 'txakolina' ); ?></h3>
@@ -22,14 +22,8 @@
         </div>
         <?php } ?>
 
-		<div class="site-info">
-			<?php if ( ! get_theme_mod( 'txakolina_footer_text' ) ) { ?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'txakolina' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s.', 'txakolina' ), 'WordPress' ); ?></a>
-				<?php printf( esc_html__( '%1$s Theme, %3$s by %2$s.', 'txakolina' ), 'txakolina', '<a href="' . esc_url( 'http://wp-themes.it/' ) . '" rel="designer">Pasquale Bucci</a>', 'crafted with <i id="cuore" class="fa fa-heart animated infinite pulse"></i>' ); ?>
-			<?php } else { ?>
-				<?php echo esc_html( get_theme_mod( 'txakolina_footer_text' ) ); ?>
-			<?php } ?>
-        </div><!-- .site-info -->
+		<div class="site-info">Site Info div here	
+    </div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
