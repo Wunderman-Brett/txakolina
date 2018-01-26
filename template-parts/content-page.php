86e8c55058bef5ae7d78ea8260aab54b92118a16
Peter Mumford <peter.mumford@fmc-na.com>
2018-01-26 14:16:05 -0500

    added sidebar widgets as custom fields, typographic changes

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -17,18 +17,31 @@
 		</header><!-- .entry-header -->
 	<?php } ?>
 
-	<div class="entry-content">
+	<div class="">
 		<div class="row">
 			<?php if (have_rows('right_column_boxes')) : ?>
-				<div class="col-md-9 col-sm-8">
+				<div class="col-md-9 col-sm-8 entry-content">
 					<?php the_content(); ?>
 				</div>
 				<div class="col-md-3 col-sm-4">
 					<?php while (have_rows('right_column_boxes')) : the_row(); ?>
-						<h2><?php the_sub_field('box_heading') ?></h2>
+						<div class="widget right-column-box">
+							<?php
+							$image = get_sub_field('box_thumbnail');
+							if ($image) :
+								echo wp_get_attachment_image($image, 'medium');
+							endif; ?>
+							<div class="widget-content">
+								<h4 class="widget-title"><?php the_sub_field('box_heading') ?></h4>
+								<?php the_sub_field('box_text'); ?>
+								<a class="btn btn-action" href="<?php the_sub_field('box_cta_link'); ?>" target="_blank" rel="noopener"><?php the_sub_field('box_cta_label'); ?></a>
+							</div>
+						</div>
+
+					<?php endwhile; ?>
 				</div>
-			<?php endwhile; else : ?>
-				<div class="col-md-12">
+			<?php else : ?>
+				<div class="col-md-12 entry-content">
 					<?php the_content(); ?>
 				</div>
 			<?php endif; ?>
