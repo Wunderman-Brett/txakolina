f37757de70144f91031668e3d9ddc9703a54fc6e
Andy McFarland <andy.mcfarland@kbmg.com>
2018-01-26 15:44:27 -0600

    partner bottom nav init commit

---
+++ partner-landing.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Partner Landing Page
+ *
+ * @package txakolina
+ */
+
+get_header(); ?>
+<div id="content" class="site-content container">
+
+    <div class="row">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main col-md-9" role="main">
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div>
+
+    <?php get_template_part('template-parts/partner','bottom_nav'); ?>
+</div>
+<?php get_footer(); ?>
---
+++ template-parts/partner-bottom_nav.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template part for Partner's bottom navigation.
+ *
+ *
+ * @package txakolina
+ */
+
+?>
+<div class="partner-bottom-nav">
+    <div class="partner-bottom-nav-header-wrapper">
+        <h3 class="partner-bottom-nav-title">Bottom <span>Navigation</span> Header</h3>
+        <p class="partner-bottom-nav-snip">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="partner-bottom-nav-wrapper">
+        <div class="partner-bottom-nav-element">
+            <div class="partner-bottom-nav-element-inner-wrapper">
+                <img src="http://placecorgi.com/60/60" alt="">
+                <h4>About NCP</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+        <div class="partner-bottom-nav-element">
+            <div class="partner-bottom-nav-element-inner-wrapper">
+                <img src="http://placecorgi.com/60/60" alt="">
+                <h4>STARTING YOUR OWN CATH LAB </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+        <div class="partner-bottom-nav-element">
+            <div class="partner-bottom-nav-element-inner-wrapper">
+                <img src="http://placecorgi.com/60/60" alt="">
+                <h4>LAB FACILITIES</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+        <div class="partner-bottom-nav-element">
+            <div class="partner-bottom-nav-element-inner-wrapper">
+                <img src="http://placecorgi.com/60/60" alt="">
+                <h4>Careers</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="partner-social">
+    <div class="partner-social-inner-wrapper">
+        <h3 class="partner-social-cta-text">Follow National Cardiovascular Partners</h3>
+        <div class="partner-social-icon-wrapper">
+            <img class="partner-social-icon-left" src="http://placecorgi.com/60/60" alt="">
+            <img class="partner-social-icon-right" src="http://placecorgi.com/60/60" alt="">
+        </div>
+    </div>
+</div>
