ca3ae37b85bb43ac6d7c19a5c0bbb32512023cc2
Brett Gaither <brett.gaither@kbmg.com>
2018-01-29 15:39:17 -0600

    hero commit

--- partner-landing.php
+++ partner-landing.php
@@ -10,7 +10,30 @@ get_header(); ?>
 
     <div class="row">
         <div id="primary" class="content-area">
-            <main id="main" class="site-main col-md-9" role="main">
+            <main id="main" class="site-main" role="main">
+                <header class="hero blue-overlay">
+                    <div class="hero-wrapper">
+                        <div class="hero-left-wrapper alignleft">
+                            <div class="hero-breadcrumbs-container">
+                                <?php
+                                    echo do_shortcode( '[breadcrumb]' ); 
+                                ?>
+                            </div>
+                            <div class="hero-logo-container">
+                                <img class="hero-partner-logo" src="http://placecorgi.com/70/40" alt="">
+                                <span class="hero-partner-name">National Cardiovascular Partners</span>
+                            </div>
+                            <div class="hero-text-container">
+                                <h1>LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT.</h1>
+                            </div>
+                        </div>
+                        <div class="hero-right-wrapper alignright">
+                            <div class="hero-cta-container">
+
+                            </div>
+                        </div>
+                    </div>
+                </header>
 
             </main><!-- #main -->
         </div><!-- #primary -->
