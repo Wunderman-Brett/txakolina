2e86d271b77428bc827a41903fff8b47c8f1a4b2
Brett Gaither <brett.gaither@kbmg.com>
2018-01-31 11:48:30 -0600

    Article Detail Container Section
    1st Revision

---
+++ template-parts/partner-article_detail_content.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template part for Partner's Article Detail Content Section.
+ *
+ *
+ * @package txakolina
+ */
+
+?>
+<div class="ArticleContentContainer row">
+    <div class="adcs-col adcs-col0">
+        <h2>Lorem Ipsum Dolor</h2><br/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+consequat. Cras ut nisl et eros imperdiet aliquam eget eu ipsum. Phasellus venenatis ex vel massa faucibus, sed 
+lacinia nisi vehicula.</p><br/>
+        <p>Nunc a egestas tortor. Morbi quam augue, euismod in suscipit in, consequat in ante. Nam aliquet, est a eleifend 
+tincidunt, velit est lacinia felis, a commodo dolor dolor quis neque. Aliquam nec commodo augue, ut rhoncus lectus. 
+Vestibulum imperdiet consectetur velit sit amet commodo. Nulla ut interdum lorem.</p><br/>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
+    </div>
+    <div class="adcs-col adcs-col1">
+        <div class="quoteCont">
+            <img class="first-quote" src="http://vccw.test/wp-content/uploads/2018/01/quote-mark-left.jpg" alt="Open Quote"/>    
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <img class="last-quote" src="http://vccw.test/wp-content/uploads/2018/01/quote-mark-right.jpg" alt="Close Quote"/>
+            <br/>
+        </div>
+        <div class="bioCont">
+        <br/>    
+        <img src="http://vccw.test/wp-content/uploads/2018/01/HPheadshot.jpg" alt="Doctor Headshot"/>
+            <br/><br/>
+            <p>-Name</p>
+            <a href="#">Read More ><a>
+        </div>
+    </div>
+</div>
