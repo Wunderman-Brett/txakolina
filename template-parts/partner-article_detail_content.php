58a128878d833856aebf2162231b49496614d268
Brett Gaither <brett.gaither@kbmg.com>
2018-01-31 15:30:18 -0600

    Content Detail Section Update (1.31.18 3:29pm)
    Revision 2

--- template-parts/partner-article_detail_content.php
+++ template-parts/partner-article_detail_content.php
@@ -13,12 +13,12 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore 
 magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
 consequat. Cras ut nisl et eros imperdiet aliquam eget eu ipsum. Phasellus venenatis ex vel massa faucibus, sed 
-lacinia nisi vehicula.</p><br/>
+lacinia nisi vehicula.</p>
         <p>Nunc a egestas tortor. Morbi quam augue, euismod in suscipit in, consequat in ante. Nam aliquet, est a eleifend 
 tincidunt, velit est lacinia felis, a commodo dolor dolor quis neque. Aliquam nec commodo augue, ut rhoncus lectus. 
-Vestibulum imperdiet consectetur velit sit amet commodo. Nulla ut interdum lorem.</p><br/>
+Vestibulum imperdiet consectetur velit sit amet commodo. Nulla ut interdum lorem.</p>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/>
+occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     <div class="adcs-col adcs-col1">
         <div class="quoteCont">
